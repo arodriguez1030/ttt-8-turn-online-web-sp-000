1849e5e73f28e86d87bfcd8b006a0e3028990485
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-03 21:11:16 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,7 +31,7 @@ def turn(board)
   input= gets.strip
   index = input_to_index(input)
   until valid_move?(board,index) == false
-    move(board, index, value = "X")
+    move(board, index)
     display_board(board)
 end
 end
